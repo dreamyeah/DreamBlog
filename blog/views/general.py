9022e9c15ff8421166a56eca3d4e7df7585176e7
gavin <gavin@163.com>
2016-09-23 22:28:33 +0800

    add

--- blog/views/general.py
+++ blog/views/general.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template, session, redirect, url_for, \
-     request, flash, g, jsonify, abort
+     request, flash, g, jsonify, abort, make_response
 from flask_openid import COMMON_PROVIDERS
 from blog import oid
 from blog.search import search as perform_search
 from blog.utils import requires_login, request_wants_json
 from blog.database import db_session, User
 from blog.listings.releases import releases
-
+import os
+import re
+import json
+from blog.uploader import Uploader
+from blog import app
 mod = Blueprint('general', __name__)
 
 
@@ -150,3 +154,136 @@ def blogindex2():
     return render_template(
         'blog/index2.html'
     )
+    
+@mod.route('/uedit')
+def uedit():
+    return render_template(
+        'blog/uedit.html'
+    )
+    
+@mod.route('/create')
+def create():
+    return render_template(
+        'blog/create.html'
+    )
+@mod.route('/upload/', methods=['GET', 'POST', 'OPTIONS'])
+def upload():
+
+    mimetype = 'application/json'
+    result = {}
+    action = request.args.get('action')
+
+    with open(os.path.join(app.static_folder, 'ueditor', 'php',
+                           'config.json')) as fp:
+        try:
+
+            CONFIG = json.loads(re.sub(r'\/\*.*\*\/', '', fp.read()))
+        except:
+            CONFIG = {}
+
+    if action == 'config':
+
+        result = CONFIG
+
+    elif action in ('uploadimage', 'uploadfile', 'uploadvideo'):
+
+        if action == 'uploadimage':
+            fieldName = CONFIG.get('imageFieldName')
+            config = {
+                "pathFormat": CONFIG['imagePathFormat'],
+                "maxSize": CONFIG['imageMaxSize'],
+                "allowFiles": CONFIG['imageAllowFiles']
+            }
+        elif action == 'uploadvideo':
+            fieldName = CONFIG.get('videoFieldName')
+            config = {
+                "pathFormat": CONFIG['videoPathFormat'],
+                "maxSize": CONFIG['videoMaxSize'],
+                "allowFiles": CONFIG['videoAllowFiles']
+            }
+        else:
+            fieldName = CONFIG.get('fileFieldName')
+            config = {
+                "pathFormat": CONFIG['filePathFormat'],
+                "maxSize": CONFIG['fileMaxSize'],
+                "allowFiles": CONFIG['fileAllowFiles']
+            }
+
+        if fieldName in request.files:
+            field = request.files[fieldName]
+            uploader = Uploader(field, config, mod.static_folder)
+            result = uploader.getFileInfo()
+        else:
+            result['state'] = 'upload API error'
+
+    elif action in ('uploadscrawl'):
+
+        fieldName = CONFIG.get('scrawlFieldName')
+        config = {
+            "pathFormat": CONFIG.get('scrawlPathFormat'),
+            "maxSize": CONFIG.get('scrawlMaxSize'),
+            "allowFiles": CONFIG.get('scrawlAllowFiles'),
+            "oriName": "scrawl.png"
+        }
+        if fieldName in request.form:
+            field = request.form[fieldName]
+            uploader = Uploader(field, config, app.static_folder, 'base64')
+            result = uploader.getFileInfo()
+        else:
+            result['state'] = 'upload API error'
+
+    elif action in ('catchimage'):
+        config = {
+            "pathFormat": CONFIG['catcherPathFormat'],
+            "maxSize": CONFIG['catcherMaxSize'],
+            "allowFiles": CONFIG['catcherAllowFiles'],
+            "oriName": "remote.png"
+        }
+        fieldName = CONFIG['catcherFieldName']
+
+        if fieldName in request.form:
+
+            source = []
+        elif '%s[]' % fieldName in request.form:
+
+            source = request.form.getlist('%s[]' % fieldName)
+
+        _list = []
+        for imgurl in source:
+            uploader = Uploader(imgurl, config, app.static_folder, 'remote')
+            info = uploader.getFileInfo()
+            _list.append({
+                'state': info['state'],
+                'url': info['url'],
+                'original': info['original'],
+                'source': imgurl,
+            })
+
+        result['state'] = 'SUCCESS' if len(_list) > 0 else 'ERROR'
+        result['list'] = _list
+
+    else:
+        result['state'] = 'request address error'
+
+    result = json.dumps(result)
+
+    if 'callback' in request.args:
+        callback = request.args.get('callback')
+        if re.match(r'^[\w_]+$', callback):
+            result = '%s(%s)' % (callback, result)
+            mimetype = 'application/javascript'
+        else:
+            result = json.dumps({'state': 'callback args error'})
+
+    res = make_response(result)
+    res.mimetype = mimetype
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,X_Requested_With'
+    return redirect('blog/create2.html',post_content=res)
+
+
+@mod.route('/addpost', methods=['POST'])
+def addpost():
+    if request.method == 'POST':
+
+        return render_template('blog/create2.html',content=request.form['content'])
